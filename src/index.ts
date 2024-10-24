676f6fb0c97ed2d6d434023324515bcd106807af
Batromium <batromium@gmail.com>
2024-10-23 23:23:45 -0500

    Initial Commit

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+var rickroll: string = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+
+function ClickedButton() {
+    alert("Why did you click the button...");
+    window.location.href = rickroll;
+}
